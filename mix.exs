8bd55bcc49de2c80e290c7258099b756cc84cd13
Marcelo Gornstein <marcelog@gmail.com>
2017-01-09 08:49:56 -0300

    bumping up version

--- mix.exs
+++ mix.exs
@@ -9,7 +9,7 @@ defmodule ElixirAgi.Mixfile do
       package: package,
       source_url: "https://github.com/marcelog/elixir_agi",
       app: :elixir_agi,
-      version: "0.0.13",
+      version: "0.0.14",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
       deps: deps
